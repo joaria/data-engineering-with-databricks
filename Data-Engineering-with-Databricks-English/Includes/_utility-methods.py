0bab04f672d7ac8cd30990ef404dbe43224e7b9d
joaria <joaria>
2022-08-08 17:52:54 +0000

    changed dependencies for pip install

--- Data-Engineering-with-Databricks-English/Includes/_utility-methods.py
+++ Data-Engineering-with-Databricks-English/Includes/_utility-methods.py
@@ -2,7 +2,7 @@
 # MAGIC %pip install \
 # MAGIC git+https://github.com/databricks-academy/dbacademy-gems \
 # MAGIC git+https://github.com/databricks-academy/dbacademy-rest \
-# MAGIC --quiet --disable-pip-version-check
+# MAGIC --quiet --disable-pip-version-check --no-deps
 
 # COMMAND ----------
 
